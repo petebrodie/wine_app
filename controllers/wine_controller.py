bbe4913c788a7216f9c004d6757a76cfa31a72f3
Pete <peterwbrodie@gmail.com>
2021-03-23 13:13:10 +0000

    starting on templates

--- controllers/wine_controller.py
+++ controllers/wine_controller.py
@@ -5,5 +5,16 @@ from models.producer import Producer
 import repositories.wine_repository as wine_repository
 import repositories.producer_repository as producer_repository
 
-wine_blueprint = Blueprint("wine", __name__)
+wine_blueprint = Blueprint("wines", __name__)
 
+@wine_blueprint.route("/wines")
+def wine():
+    wine = wine_repository.select_all()
+    return render_template("wines/index.html", all_wines = wine)
+
+
+# NEW GET WINE ('/wine/new')
+@wines_blueprint.route("/wines/new", methods=["GET"])
+def new_wine():
+    producer = producer_repository.select_all()
+    return render_template("wines/new.html", all_producers = producers)
