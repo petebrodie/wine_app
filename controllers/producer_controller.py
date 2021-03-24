45e4b3a4975b007baebb0cb1530e184223980fe1
Pete <peterwbrodie@gmail.com>
2021-03-24 07:25:28 +0000

    create producer in controller

--- controllers/producer_controller.py
+++ controllers/producer_controller.py
@@ -20,10 +20,36 @@ def new_producer():
     return render_template("producer/new.html", wines=wines)
 
 
+@producer_blueprint.route("/producers", methods=["POST"])
+def create_producer():
+    name = request.form["name"]
+    phone_number = request.form["phone_number"]
+    email = request.form["email"]
+    country = request.form["country"]
+    region = request.form["region"]
+
+    wine = wine_repository.select(wine_id)
+    producer = Producer(
+        name,
+        phone_number,
+        email,
+        country,
+        region)
+    producer_repository.save.(producer)
+    return redirect("/producers")
 
 
 
-# @wine_blueprint.route("/wines/new", methods=["GET"])
-# def new_wine():
+
+# # create a route that handles a post request /wines which will create a new wine object and save it to the databas
+
+# @wine_blueprint.route('/wines/<id>/delete', methods=['POST'])
+# def delete_wine(id):
+#     wine_repository.delete(id)
+#     return redirect('/wines')
+
+# @wine_blueprint.route('/wines/<id>/edit', methods=['POST'])
+# def edit_wine(id):
+#     wine =wine_repository.select(id)
 #     producers = producer_repository.select_all()
-#     return render_template("wine/new.html", producers = producers)
+#     return render_template('/wine/edit.html', wine=wine, producers=producers)
--- models/producer.py
+++ models/producer.py
@@ -1,6 +1,6 @@
 class Producer:
     
-    def __init__(self, name, phone_number, email, country, region, id = None):
+    def __init__(s):elf, name, phone_number, email, country, region, id = None
         self.id = id
         self.name = name
         self.phone_number = phone_number
